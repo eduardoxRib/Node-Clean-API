0da29a13a251bca2034e21b3af23cbacc3ea9c0b
Eduardo Ribeiro <eduardoxrib@gmail.com>
2021-11-02 00:40:55 -0300

    feat: add get collection for mongo helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb'
+import { Collection, MongoClient } from 'mongodb'
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -9,5 +9,9 @@ export const MongoHelper = {
 
   async disconnect (): Promise<void> {
     await this.client.close()
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    return this.client.db().collection(name)
   }
 }
