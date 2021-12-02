3c0be4267b2856276d2491b3eb96410fdf733cdd
Eduardo Ribeiro <eduardoxrib@gmail.com>
2021-12-01 21:19:17 -0300

    feat: ensure LogMongoRepository creates an error log on throws

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -13,7 +13,7 @@ export class LogControllerDecorator implements Controller {
   async handle (HttpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(HttpRequest)
     if (httpResponse.statusCode === 500) {
-      await this.logErrorRepository.log(httpResponse.body.stack)
+      await this.logErrorRepository.logError(httpResponse.body.stack)
     }
     return httpResponse
   }
