c8325991b167e032e108b7ee1eb065613470cc4d
Eduardo Ribeiro <eduardoxrib@gmail.com>
2021-12-06 22:39:45 -0300

    refactor: move sut creation to a factory

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -2,9 +2,20 @@ import { badRequest } from '../../helpers/http-helper'
 import { LoginController } from './login'
 import { MissingParamError } from '../../errors/missing-param-error'
 
+interface SutTypes {
+  sut: LoginController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoginController()
+  return {
+    sut
+  }
+}
+
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
-    const sut = new LoginController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -15,7 +26,7 @@ describe('Login Controller', () => {
   })
 
   test('Should return 400 if no password is provided', async () => {
-    const sut = new LoginController()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email'
